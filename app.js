6a4c5293b6e2fc108689231e7eb416349d49f52b
bresettem <bresettem@users.noreply.github.com>
2024-01-05 16:55:10 -0600

    Level 1 Username and Password Only

--- app.js
+++ app.js
@@ -2,8 +2,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("moongose");
 const app = express();
+const pg = require("pg");
+
+const db = new pg.Client({
+  user: "brese",
+  host: "localhost",
+  database: "userdb",
+  password: "Hello123",
+  port: 5432,
+});
+
+db.connect();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -13,7 +23,6 @@ app.use(
   })
 );
 
-mongoose.connect("mongodb://localhost:27017/userDB", { usenewUrlParser: true });
 app.get("/", function (req, res) {
   res.render("home");
 });
@@ -24,6 +33,62 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    const results = await db.query("SELECT * FROM users WHERE email = $1", [
+      username,
+    ]);
+    const user = results.rows[0];
+
+    if (user) {
+      res.render("register", {
+        message: "The email already exists, try again.",
+      });
+    } else {
+      await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [
+        username,
+        password,
+      ]);
+      res.render("secrets");
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.render("register", {
+      message: "Server is not connected, please try again later.",
+    });
+  }
+});
+
+app.post("/login", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const results = await db.query("SELECT * FROM users WHERE email = $1", [
+      username,
+    ]);
+    const user = results.rows[0];
+    if (user) {
+      if (password == user.password) {
+        res.render("secrets");
+      } else {
+        res.render("login", {
+          message: "The password is not correct, try again.",
+        });
+      }
+    } else {
+      res.render("login", {
+        message: "The username is not correct, try again.",
+      });
+    }
+  } catch (error) {
+    console.error(error.detail);
+    res.render("login", { message: "An error occured, please try again." });
+  }
 });
 
 app.listen(3000, function () {
